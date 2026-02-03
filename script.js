15efa32da7fd6df7bde0879bab46916efafb6603
Yeike23 <alexisgarabito14@gmail.com>
2026-02-03 16:30:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const scene = document.querySelector(".scene");
+
+function createHeart() {
+  const heart = document.createElement("div");
+  heart.classList.add("heart");
+
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.animationDuration = 4 + Math.random() * 4 + "s";
+  heart.style.opacity = Math.random();
+
+  scene.appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 8000);
+}
+
+setInterval(createHeart, 200);
 
